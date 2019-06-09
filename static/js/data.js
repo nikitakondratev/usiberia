16c0d8f5e045f44b5cef592bcd4095680f14f25c
nikitakondratev <kondr012@gmail.com>
2019-06-09 11:52:58 +0700

    Create data.js

---
+++ static/js/data.js
@@ -0,0 +1,4 @@
+var codropsEvents = {
+	'03-21-2019' : '<a href="http://tympanus.net/codrops/2012/03/21/psd2html-giveaway/">PSD2HTML Giveaway</a>',
+	'04-26-2019' : '<span>Защита проекта</span>'
+};
